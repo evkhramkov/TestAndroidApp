3dc7b33c2056c384b1ed4ca043f68133d8d646fe
Evgeniy Khramkov <v-evkhra@microsoft.com>
2017-05-05 14:53:40 +0300

    expresso tests init

--- app/src/androidTest/java/com/example/ewgenius/testandroidapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/ewgenius/testandroidapp/ExampleInstrumentedTest.java
@@ -4,8 +4,13 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.After;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import android.support.test.runner.AndroidJUnit4;
+import com.xamarin.testcloud.espresso.Factory;
+import com.xamarin.testcloud.espresso.ReportHelper;
 
 import static org.junit.Assert.*;
 
@@ -16,11 +21,19 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+    public ReportHelper reportHelper = Factory.getReportHelper();
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.ewgenius.testandroidapp", appContext.getPackageName());
+    }
+
+    @After
+    public void TearDown(){
+        reportHelper.label("Stopping App");
     }
 }
