d57782208fff777b3cc0d896bfb77443f1500f85
Evgeniy Khramkov <v-evkhra@microsoft.com>
2017-05-05 14:40:43 +0300

    mobile center init

--- app/src/main/java/com/example/ewgenius/testandroidapp/MainActivity.java
+++ app/src/main/java/com/example/ewgenius/testandroidapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.ewgenius.testandroidapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.microsoft.azure.mobile.MobileCenter;
+import com.microsoft.azure.mobile.analytics.Analytics;
+import com.microsoft.azure.mobile.crashes.Crashes;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MobileCenter.start(getApplication(), "ac7f7411-57d1-4536-ab64-2af0485746ca",
+                Analytics.class, Crashes.class);
     }
 }
