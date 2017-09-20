21042791f9a10c3b5c64d3eeba2e109b8880e675
Evgeniy Khramkov <v-evkhra@microsoft.com>
2017-09-20 21:17:22 +0300

    Update mobile-center-post-clone.sh

--- app/mobile-center-post-clone.sh
+++ app/mobile-center-post-clone.sh
@@ -1,5 +1,2 @@
 echo "current dir is $PWD"
-
-echo "USER-DEFINED_USERNAME $USER-DEFINED_USERNAME"
-echo "USER_DEFINED_USERNAME $USER_DEFINED_USERNAME"
-echo "passwrod $password"
+echo "username $username passwrod $password"
